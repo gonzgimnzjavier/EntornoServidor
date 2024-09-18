643a08b4bb9f16913a1efa95dd9e4bd834724aa1
Javier <javigongimenez@gmail.com>
2024-09-18 15:41:26 +0200

    Primer commit con archivos correctos

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre:");
+
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Bienvenido "+ nombre);
+        }
+    }
+}
